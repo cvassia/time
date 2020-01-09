db41196f6913530b8209bd48581ae06afa4ab475
vassia <vassiachrist@mbp-vassia.localdomain>
2020-01-09 11:53:05 +0100

    new

---
+++ precache-manifest.0eedf10296754f4c4e2a2e8c25210374.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bacef5a9ffd5e9f55e25f125c19b3991",
+    "url": "/cvassia/time.git/index.html"
+  },
+  {
+    "revision": "e1907808afb084702969",
+    "url": "/cvassia/time.git/static/css/main.11b8d18c.chunk.css"
+  },
+  {
+    "revision": "56da178fd63136c15c72",
+    "url": "/cvassia/time.git/static/js/2.c2946dab.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cvassia/time.git/static/js/2.c2946dab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e1907808afb084702969",
+    "url": "/cvassia/time.git/static/js/main.633a9e98.chunk.js"
+  },
+  {
+    "revision": "27c83704a8b96fb21c04",
+    "url": "/cvassia/time.git/static/js/runtime-main.a599d91a.js"
+  },
+  {
+    "revision": "52d08540910a9732e624f8682807f8fd",
+    "url": "/cvassia/time.git/static/media/Hypnotic Clock Sound.52d08540.m4a"
+  }
+]);
